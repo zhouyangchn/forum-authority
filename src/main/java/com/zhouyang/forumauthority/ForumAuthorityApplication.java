9966bfc0e4d5096555d5443bc70f064a575b7683
zhouyang <zhouyang_chn@163.com>
2021-08-24 02:35:20 +0800

    init

---
+++ src/main/java/com/zhouyang/forumauthority/ForumAuthorityApplication.java
@@ -0,0 +1,13 @@
+package com.zhouyang.forumauthority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumAuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumAuthorityApplication.class, args);
+    }
+
+}
